2f5ae27eec1c2778af9f194b075ba470525f6338
Alexander Alasjö <alexander@alasjo.se>
2020-11-30 20:00:58 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Advent_of_Code_2020
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
